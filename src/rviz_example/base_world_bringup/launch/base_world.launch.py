2040b210c0a70ca32816615956304ef644108b69
Edward Liao <edw0527.eic07g@nctu.edu.tw>
2021-04-15 13:28:30 +0800

    Add rviz_example base_world_bringup

---
+++ src/rviz_example/base_world_bringup/launch/base_world.launch.py
@@ -0,0 +1,17 @@
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    pkg_share = FindPackageShare('base_world_bringup').find('base_world_bringup')
+    urdf_file = os.path.join(pkg_share, 'urdf', 'basic_world.urdf')
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+    rsp_params = {'robot_description': robot_desc}
+    return LaunchDescription([
+        Node(package='laser_scan_publisher', executable='laser_scan_pub', output='screen'),
+        Node(package='robot_state_publisher', executable='robot_state_publisher',
+             output='screen', parameters=[rsp_params]),
+    ])
